647813b5e538c11b36fc1dca6689cf65d8ed61c9
danielcev <daniel_plata_apps1m1617@cev.com>
2017-03-24 13:32:52 +0100

    Añadida Toolbar en todas las Activities

--- app/src/main/java/com/daniel/hnd2/activities/EditActivity.java
+++ app/src/main/java/com/daniel/hnd2/activities/EditActivity.java
@@ -3,6 +3,7 @@ package com.daniel.hnd2.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,10 +21,15 @@ public class EditActivity extends AppCompatActivity implements View.OnClickListe
     private Button btn_cambios;
     private String foto;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         editNombre = (EditText) findViewById(R.id.editNombre);
         editApellidos = (EditText) findViewById(R.id.editApellidos);
--- app/src/main/java/com/daniel/hnd2/activities/RegistroActivity.java
+++ app/src/main/java/com/daniel/hnd2/activities/RegistroActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,10 +19,15 @@ public class RegistroActivity extends AppCompatActivity implements View.OnClickL
     private EditText editNombre, editApellidos, editUsuario, editPassword;
     private Button btn_registro;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         editNombre = (EditText) findViewById(R.id.editNombre);
         editApellidos = (EditText) findViewById(R.id.editApellidos);
